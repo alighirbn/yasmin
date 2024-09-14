f874d117ec6c9cc7df9dfb8b4aa572da81ecc9da
alighirbn <alighirbn@gmail.com>
2024-09-14 17:45:22 +0300

    notification error fix

--- resources/views/notification/index.blade.php
+++ resources/views/notification/index.blade.php
@@ -12,21 +12,27 @@
                     <br>
 
                     <ul>
-                        @if (auth()->user()->unreadNotifications()->groupBy('notifiable_type')->count() != 0)
+                        @php
+                            $unreadCount = auth()->user()->unreadNotifications()->count();
+                            $readCount = auth()->user()->readNotifications()->count();
+                        @endphp
+
+                        @if ($unreadCount > 0)
                             <li>
                                 <a rel="alternate" href="{{ route('notification.markallasread') }}"
-                                    class="block w-full px-4 py-2  text-right leading-5 text-green-600 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
+                                    class="block w-full px-4 py-2 text-right leading-5 text-green-600 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
                                     <span class="w-full px-4 py-2 font-bold bg-success text-white rounded">
-                                        {{ __('word.markallasread') }}</span>
+                                        {{ __('word.markallasread') }}
+                                    </span>
                                 </a>
                             </li>
 
                             <br>
                             <li>
                                 <a rel="alternate"
-                                    class="block w-full px-4 py-2  text-right leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
+                                    class="block w-full px-4 py-2 text-right leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
                                     {{ __('word.unreadnotification') }}
-                                    {{ auth()->user()->unreadNotifications()->groupBy('notifiable_type')->count() }}
+                                    {{ $unreadCount }}
                                 </a>
                             </li>
                         @endif
@@ -35,13 +41,13 @@
                             <li class="border border-solid">
                                 <a rel="alternate" href="{{ route('notification.markasread', $notification) }}"
                                     class="block w-full px-4 py-2 text-lg text-right leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
-                                    {{ $notification->data['name'] . ' - ' . $notification->data['action'] . ' - وقت الاشعار - ' . $notification->created_at }}
+                                    {{ $notification->data['name'] . ' - ' . $notification->data['action'] . ' - وقت الاشعار - ' . \Carbon\Carbon::parse($notification->created_at)->format('Y-m-d H:i:s') }}
                                 </a>
                             </li>
                         @empty
                             <li>
                                 <a rel="alternate"
-                                    class="block w-full px-4 py-2  text-right leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
+                                    class="block w-full px-4 py-2 text-right leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
                                     {{ __('word.nonotification') }}
                                 </a>
                             </li>
@@ -50,25 +56,21 @@
 
                         <li>
                             <a rel="alternate"
-                                class="block w-full px-4 py-2  text-right leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
+                                class="block w-full px-4 py-2 text-right leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
                                 {{ __('word.readnotification') }}
-                                {{ auth()->user()->readNotifications()->groupBy('notifiable_type')->count() }}
+                                {{ $readCount }}
                             </a>
                         </li>
                         @forelse (auth()->user()->readNotifications()->take(500)->get() as $notification)
                             <li class="border border-solid">
                                 <a rel="alternate" href="{{ route('notification.markasread', $notification) }}"
                                     class="block w-full px-4 py-2 text-lg text-right leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
-                                    {{ $notification->data['name'] . ' - ' . $notification->data['action'] . ' - وقت الاشعار - ' . $notification->created_at . ' - وقت فتح الاشعار - ' . $notification->read_at }}
+                                    {{ $notification->data['name'] . ' - ' . $notification->data['action'] . ' - وقت الاشعار - ' . \Carbon\Carbon::parse($notification->created_at)->format('Y-m-d H:i:s') . ' - وقت فتح الاشعار - ' . \Carbon\Carbon::parse($notification->read_at)->format('Y-m-d H:i:s') }}
                                 </a>
                             </li>
                         @empty
                         @endforelse
-
-
                     </ul>
-
-
                 </div>
             </div>
         </div>
